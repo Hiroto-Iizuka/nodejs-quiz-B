df6f2f4d9bea194bedb4cc51a6fd0a628209ee62
Hiroto-Iizuka <oroxith@gmail.com>
2022-11-03 21:43:05 +0900

    fix: routerを整備

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const router = require('./router/index');
+const indexRouter = require('./routes/index');
+const quizRouter = require('./routes/quiz');
 
 const app = express();
 const PORT = 3001;
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.use('/', router);
+app.use('/', indexRouter);
+app.use('/quiz', quizRouter);
+
 app.listen(PORT, () => {
   console.log(`Quiz app listening on port ${PORT}!`);
 });
