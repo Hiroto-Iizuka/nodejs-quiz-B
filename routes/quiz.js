42e565b039c7b501715458877d07589729ed4768
Hiroto-Iizuka <oroxith@gmail.com>
2022-11-08 21:49:09 +0900

    feat: クイズを取得するAPIを実装

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
 
-router.get('/', (req, res) => {
-  res.send("hello");
+router.get('/', async (req, res, next) => {
+  // QUIZオブジェクトの取得
+  const QUIZ_API = 'https://opentdb.com/api.php?amount=10&type=multiple';
+
+  try {
+    const response = await fetch(QUIZ_API);
+    const data = await response.json()
+    res.send(data.results);  
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
