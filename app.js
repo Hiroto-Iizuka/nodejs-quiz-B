046b06f221191143b6b90dcbce78719460bdac86
Hiroto-Iizuka <oroxith@gmail.com>
2022-11-09 21:35:53 +0900

    feat: フロントを実装

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
+const path = require('path');
 const indexRouter = require('./routes/index');
 const quizRouter = require('./routes/quiz');
 
 const app = express();
 const PORT = 3001;
 
-// app.set('view engine', 'ejs');
+app.set("view engine", "html");
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/quiz', quizRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index');
-});
+router
+  .get('/', (req, res) => {
+    res.render('index');
+  })
+  .get('/', (req, res) => {
+    res.render('quiz');
+  });
 
 module.exports = router;
