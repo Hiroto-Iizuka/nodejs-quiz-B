2cb3e7861380901830781645695e670b7fb472a2
Hiroto-Iizuka <oroxith@gmail.com>
2022-11-19 10:12:09 +0900

    fix: シャッフル後の４択解答をバックエンド側で生成

--- routes/quiz.js
+++ routes/quiz.js
@@ -9,11 +9,42 @@ router.get('/', async (req, res, next) => {
   try {
     const response = await fetch(QUIZ_API);
     const data = await response.json();
+    const quizzes = JSON.parse(JSON.stringify(data.results));
+    console.log(quizzes[0]);
+    quizzes.map (
+      quiz => {
+        const answers = buildAnswers(quiz);
+        quiz.answers = answers;
+      }
+    );
     res.header('Content-Type', 'application/json; charset=urf-8');
-    res.send(data.results);
+    res.send(quizzes);
   } catch (error) {
     next(error);
   }
 });
+
+  // シャッフル済みの解答一覧を取得する
+  const buildAnswers = (quiz) => {
+    const answers = [
+      quiz.correct_answer,
+      ...quiz.incorrect_answers
+    ];
+
+    const shuffledAnswers = shuffle(answers);
+
+    return shuffledAnswers;
+  };
+
+  // 解答をシャッフル
+  const shuffle = (array) => {
+    const copiedArray = array.slice();
+    for (let i = copiedArray.length - 1; i >= 0; i--){
+      const rand = Math.floor( Math.random() * ( i + 1 ) );
+      [copiedArray[i], copiedArray[rand]] = [copiedArray[rand], copiedArray[i]]
+    }
+
+    return copiedArray;
+  };
 
 module.exports = router;
